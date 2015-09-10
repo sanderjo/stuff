8a1d18bd9baaba40908e2cba93f4c237c3391978
Sander <sander.jonkers+github@gmail.com>
2015-09-10 08:22:14 +0200

    Create http-https-tester.py

---
+++ http-https-tester.py
@@ -0,0 +1,18 @@
+import urllib2
+
+urllist= [
+    'http://www.google.com/',
+    'https://www.google.com/',
+    'http://api.nzbgeek.info/api?t=get&id=xxx&apikey=yy',
+    'https://api.nzbgeek.info/api?t=get&id=xxx&apikey=yy'
+]
+
+for url in urllist:
+    try:
+        f = urllib2.urlopen(url, timeout=4)    # timeout 4 seconds, in case website is not accessible
+            result = f.read()[:100]
+            print "URL ", url, "OK, with result", result
+    except:
+            print "URL ", url, "not OK"
+
+print "Finished"
