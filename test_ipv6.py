e97b5af74bbf16a7c1b2cc59a7d7e405a679e7e9
sanderjo <sander.jonkers+github@gmail.com>
2015-09-16 00:15:58 +0200

    also test with urllib2 against ipv6-only host

--- test_ipv6.py
+++ test_ipv6.py
@@ -1,6 +1,7 @@
 import socket
 import logging
 import sys
+import urllib2
 
 logger = logging.getLogger('')
 logger.setLevel(logging.DEBUG)
@@ -42,9 +43,26 @@ def test_ipv6():
 
 #######################################################
 
+def test_ipv6_urllib2(host):
+    url = 'http://' + host + '/'
+    try:
+        f = urllib2.urlopen(url, timeout=2)
+        result = f.read()[:20]
+        logging.debug('Test IPv6: success with urrlib2. %s %s',  url,result )
+    except:
+        logging.debug('Test IPv6: Problem with urllib2. Disabling IPv6. Reason: %s', sys.exc_info()[0] )
+        return False
+    return True
 
+# main:
 
 result = test_ipv6()
 print "test_ipv6() is", result
+test_ipv6_urllib2('ipv6.google.com')	# only use IPv6-only web-enabled hosts!!!
+test_ipv6_urllib2('[2a00:1450:400f:804::200e]')
+test_ipv6_urllib2('ipv6.test-ipv6.com')
+
+
+
 
 
