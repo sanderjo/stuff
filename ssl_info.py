671f45d478b7015883797a0f3a09b05cac679c47
SanderJ <sander.jonkers+github@gmail.com>
2015-10-16 17:10:10 +0200

    more ssl stuff

--- ssl_info.py
+++ ssl_info.py
@@ -5,9 +5,45 @@ import ssl
 
 print "OPENSSL_VERSION:", ssl.OPENSSL_VERSION
 print "OPENSSL_VERSION_INFO:", ssl.OPENSSL_VERSION_INFO
-print "PROTOCOL_SSLv23:", ssl.PROTOCOL_SSLv23
-print "PROTOCOL_SSLv3:", ssl.PROTOCOL_SSLv3
-print "PROTOCOL_TLSv1:", ssl.PROTOCOL_TLSv1
-print "\nModule ssl:", dir(ssl)
+
+print "\nProtocols found:"
+for i in dir(ssl):
+    #print i
+    if i.find('PROTOCOL_') == 0:
+        print i
+
+print "\nDetails:"
+#print "PROTOCOL_SSLv23:", ssl.PROTOCOL_SSLv23
+#print "PROTOCOL_SSLv3:", ssl.PROTOCOL_SSLv3
+#print "PROTOCOL_TLSv1:", ssl.PROTOCOL_TLSv1
+
+try:
+    print "PROTOCOL_SSLv23:", ssl.PROTOCOL_SSLv23
+except:
+    print "No PROTOCOL_SSLv23"
+
+try:
+    print "PROTOCOL_SSLv3:", ssl.PROTOCOL_SSLv3
+except:
+    print "No PROTOCOL_SSLv3"
+        
+try:
+    print "PROTOCOL_TLSv1:", ssl.PROTOCOL_TLSv1
+except:
+    print "No TLSv1"        
+
+
+try:
+    print "PROTOCOL_TLSv1_1:", ssl.PROTOCOL_TLSv1_1
+except:
+    print "No TLSv1_1"
+
+try:
+    print "PROTOCOL_TLSv1_2:", ssl.PROTOCOL_TLSv1_2
+except:
+    print "No TLSv1_2"
+
+
+#print "\nModule ssl:", dir(ssl)
 
 
