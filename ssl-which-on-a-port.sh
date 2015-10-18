30991c3500ad5b291bbabb00942b4dac86ea27ea
sanderjo <sander.jonkers+github@gmail.com>
2015-10-18 12:58:25 +0200

    ssl shell script

--- ssl-which-on-a-port.sh
+++ ssl-which-on-a-port.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 echo $1 
 echo $2
 
@@ -6,4 +8,5 @@ echo -n | openssl s_client -connect $1:$2 -tls1_2  2>&1 | grep -e Cipher -e Prot
 echo -n | openssl s_client -connect $1:$2 -tls1  2>&1 | grep -e Cipher -e Protocol
 echo -n | openssl s_client -connect $1:$2 -ssl3  2>&1 | grep -e Cipher -e Protocol
 echo -n | openssl s_client -connect $1:$2 -ssl2  2>&1 | grep -e Cipher -e Protocol
+
 
