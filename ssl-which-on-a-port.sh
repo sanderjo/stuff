c099e76da273001955a9125fe1397d16a76caf92
sanderjo <sander.jonkers+github@gmail.com>
2015-10-18 12:57:15 +0200

    shell script to do SSL analysis

---
+++ ssl-which-on-a-port.sh
@@ -0,0 +1,9 @@
+echo $1 
+echo $2
+
+echo -n | openssl s_client -connect $1:$2 -tls1_1  2>&1 | grep -e Cipher -e Protocol
+echo -n | openssl s_client -connect $1:$2 -tls1_2  2>&1 | grep -e Cipher -e Protocol
+echo -n | openssl s_client -connect $1:$2 -tls1  2>&1 | grep -e Cipher -e Protocol
+echo -n | openssl s_client -connect $1:$2 -ssl3  2>&1 | grep -e Cipher -e Protocol
+echo -n | openssl s_client -connect $1:$2 -ssl2  2>&1 | grep -e Cipher -e Protocol
+
